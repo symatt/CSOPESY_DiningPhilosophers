fcef55ff6d6a8949f7eedf4e366ab412f28ef34e
symatt <jamesxsy17@gmail.com>
2022-05-25 20:36:37 +0800

    initialized Activity2

---
+++ Activity2.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.*;
+public class Activity2 {
+    public static void main(String[] args) {
+        System.out.println("Driver class");
+        // create dining philosopher object
+        // instance of each philosopher
+        // start execution of each philosopher
+
+
+    }
+}
